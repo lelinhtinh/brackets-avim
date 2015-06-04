47f84bf57900cc3ea522bdabbdd56a62546dc7f1
Hong Nguyen <nghongbk@gmail.com>
2015-06-05 06:58:18 +0700

    Refactor code

--- main.js
+++ main.js
@@ -3,6 +3,8 @@
 
 define(function (require) {
     "use strict";
+    
+    require("avim");
 
     var Menus = brackets.getModule("command/Menus"),
         KeyBindingManager = brackets.getModule("command/KeyBindingManager"),
@@ -20,72 +22,92 @@ define(function (require) {
         avimMethod = avimPreferences.get("method"),
         avimShortcut = avimPreferences.get("shortcut");
     
+    // Tạo đối tượng trỏ đến menu Edit
+    var menu = Menus.getMenu(Menus.AppMenuBar.EDIT_MENU);
+    
+    // Đăng ký command COMMAND_AVIM_ON
+    var avimMenu = CommandManager.register("Bộ gõ AVIM", COMMAND_AVIM_ON, function () {
+        avimOn = avimOn ? false : true;
+        avimPreferences.set("on", avimOn);
+        avimUpdateState();
+    });
+    
     // Danh sách các kiểu gõ
     var avimLabel = ["AUTO", "TELEX", "VNI", "VIQR", "VIQR*", "---", "OFF"];
-
-    require("avim");
-
+    
+    // Tạo dropdown menu các kiểu gõ
+    var $DropdownMethod = new DropdownButton.DropdownButton("OFF", avimLabel);
+    
     /**
-     * Cập nhật thông tin bộ gõ
+     * Thiết lập thuộc tính mặc định khi lần đầu chạy bộ gõ
      */
-    function avimUpdateState() {
 
-        avimOn = avimPreferences.get("on"); // Kiểm tra tình trạng bật/tắt
-        
-        var currMethod = avimPreferences.get("method");
-
-        // Chọn kiểu gõ
-        if (avimOn) {
-            AVIMObj.setMethod(currMethod);
-            $DropdownMethod.$button.text(avimLabel[currMethod]);
-        } else {
-            AVIMObj.setMethod(-1);
-            currMethod = 6;
-        }
-
-        avimMenu.setChecked(avimOn); // Đánh dấu bật/tắt trên menu
-        
-        $DropdownMethod.$button.text(avimLabel[currMethod]); // Cập nhật thanh trạng thái kiểu gõ        
-    }
-
-    // Nếu chưa có thuộc tính bật/tắt
+    // Nếu chưa có thuộc tính bật/tắt, mặc định: bật
     if (avimOn === undefined) {
         avimOn = true;
         avimPreferences.set("on", true);
     }
 
-    // Nếu chưa có thuộc tính kiểu gõ
+    // Nếu chưa có thuộc tính kiểu gõ, mặc định: AUTO
     if (avimMethod === undefined) {
         avimPreferences.set("method", 0);
     }
-
-    // Đăng ký command COMMAND_AVIM_ON
-    var avimMenu = CommandManager.register("Bộ gõ AVIM", COMMAND_AVIM_ON, function () {
-        avimOn = avimOn? false: true;
-        avimPreferences.set("on", avimOn);
-        avimUpdateState();
-    });
-
-    var menu = Menus.getMenu(Menus.AppMenuBar.EDIT_MENU); // Gọi chức năng sửa menu
-    menu.addMenuDivider(); // Thêm ngăn cách dòng
-    menu.addMenuItem(COMMAND_AVIM_ON); // Thêm Bộ gõ AVIM vào menu
-    // Đánh dấu Bật trên menu
-    if (avimOn) {
-        avimMenu.setChecked(true);
-    }
-
-    // Nếu chưa cài đặt phím tắt
+    
+    // Nếu chưa cài đặt phím tắt, mặc định: Ctrl-Shìft-G
     if (avimShortcut === undefined) {
         avimShortcut = {
             key: "Ctrl-Shift-G"
         };
         avimPreferences.set("shortcut", avimShortcut);
     }
-    KeyBindingManager.addBinding(COMMAND_AVIM_ON, avimShortcut); // Thêm phím tắt cho command COMMAND_AVIM_ON
 
-    // Tạo dropdown menu các kiểu gõ
-    var $DropdownMethod = new DropdownButton.DropdownButton("OFF", avimLabel);
+    /**
+     * Cập nhật thông tin bộ gõ
+     */
+    
+    function avimUpdateState() {
 
+        avimOn = avimPreferences.get("on"); // Lấy trạng thái bật/tắt
+        
+        var currMethod = avimPreferences.get("method");
+
+        // Thiết lập trạng thái bộ gõ và kiểu gõ
+        if (avimOn) {
+            AVIMObj.setMethod(currMethod);
+            $DropdownMethod.$button.text(avimLabel[currMethod]);
+        } else {
+            AVIMObj.setMethod(-1);
+            currMethod = 6; // OFF
+        }
+
+        avimMenu.setChecked(avimOn); // Đánh dấu bật/tắt trên menu
+        
+        // Cập nhật thanh trạng thái kiểu gõ
+        $DropdownMethod.$button.text(avimLabel[currMethod]);
+    }
+    
+    /**
+     * Thêm bộ gõ vào menu Edit
+     */
+
+    menu.addMenuDivider(); // Thêm ngăn cách dòng
+    menu.addMenuItem(COMMAND_AVIM_ON); // Thêm Bộ gõ AVIM vào menu
+    // Đánh dấu Bật trên menu
+    if (avimOn) {
+        avimMenu.setChecked(true);
+    }
+    
+    /**
+     * Thêm phím tắt cho bộ gõ
+     */
+
+    // Thêm phím tắt cho command COMMAND_AVIM_ON
+    KeyBindingManager.addBinding(COMMAND_AVIM_ON, avimShortcut);
+
+    /**
+     * Tạo thanh trạng thái cho bộ gõ
+     */
+    
     // Gắn dropdown menu vào thanh trạng thái
     StatusBar.addIndicator("avim-status", $DropdownMethod.$button, true, "btn btn-dropdown btn-status-bar", "Chọn kiểu gõ Tiếng Việt", "status-overwrite");
 
